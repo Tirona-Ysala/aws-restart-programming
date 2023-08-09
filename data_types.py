274b7bc805e72093edf95317fd5978a375351f3c
Tirona <nomis_ihat@hotmail.com>
2023-08-10 09:53:50 +1000

    Enter your commit Message

---
+++ data_types.py
@@ -0,0 +1,33 @@
+1 # integer - whole number
+2.0 # float - number with decimal point
+True 
+False #boolean - true/false
+"Hello World" # string - any characters enclosed by quotation marks (single, double, or triple quotation marks)
+'900' # also a string
+
+
+# - Primary data types
+# owner="Simon Thai" #bash
+owner = "Simon Thai" #python does not see spaces as delimiter
+date = "August 8th 2023"
+version = 1.0
+final_version = False
+
+
+# Secondary data types - Composed of the primary data types and sometimes themselves
+#
+# Each element is separated from the other by using a comma
+[14, "Tirona", "is", "case sensitive"] # List - A collection of characters enclosed by square brackets; lists are mutable
+(14, "Tirona", "is", "case sensitive") # Tuple - An immuutable collection of characters enclosed by parenthesis
+{"aws-restart": "a 12 week course introducing leaners into the cloud", "date": "August 8th 2023", True: "A boolean data type", True: "Keys must be unique"} # Dictionary - A mutable collection of key value pairs enclosed by wavy brackets. Each key and value is linked via a colon
+
+# Create a list with 5 elements, float, integer, boolean, 2 strings
+[1.5, 50, True, "Test", "String 51"]
+
+# Create a dictionary with five keys and values
+{1: 40, "Hello": 20, "hi": 5, True: 9, "python": 2 }
+#values can be any data type, however keys are restricted to integers floats, boolean, string and tuples
+
+#Print sends values to the standard output
+print(date)
+print("My name is Python and I am a greaet programming language to learn!")
